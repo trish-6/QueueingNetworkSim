17f3d6dea30520f2e2feeb23ce0a80db80bf75dc
trish-6 <tristanlangley0@gmail.com>
2022-11-30 01:29:24 -0500

    Simulate all N values, add more plots
    All of N=1,2,3,4 are simulated in one run of the program. Plots of the difference between simulated and theoretical values (# pkts, delay through each node) are generated.

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ according to a Poisson process with rate lambda. They are routed through node 1,
 the N nodes at the next level, then through node 3, after which they exit the network. Each node is 
 modeled as a queue with exponential service time.
 
+## Output
+The program outputs numerous plots of the average number of packets in each node, and the average packet
+delay through each node, for each lambda and N. Both simulated and theoretical values are plotted and
+compared. Theoretical values are calculated by assuming that each node is an M/M/1 queue.
+
 ## Basic Implementation
 Each node is an instance of a class called 'Node', which keeps track of information like the node's ID
 (e.g. 1, 20, 21, 3), a list of packets in the node's queue, and arrival/departure times of packets.
@@ -18,15 +23,6 @@ thread for node 1, one thread for the N level 2 nodes, and one thread for node 3
 arrivals, services, and departures of packets in that node level. Once a certain number of packets pass
 through the system (e.g. 50,000), the simulation ends and the program plots average values for number of
 packets in each node and delay through each node.
-
-## Current Phase
-Implemented the entire queueing system, and calculation/plotting of average number of packets and
-average delay through each node. Manually checked for correct output with a slow arrival rate and a
-low number of packets. With higher arrival rates and number of packets, plots seem reasonable from
-comparing theoretical and simulated values.
-
-## Next Steps
-- Simulate 50,000 packets for each of N = 1,2,3,4 and compare simulated vs. theoretical results
 
 ## Requirements and Usage
 - Matplotlib required
--- proj.py
+++ proj.py
@@ -20,12 +20,10 @@ from typing import Type, Union, List, Tuple
 
 
 # ----------------------------------- Global variables -----------------------------------
-# EDIT THESE VALUES PER SIMULATION:
-# How many level 2 nodes do you want? 1, 2, 3, or 4?
-N = 4
-# How many packets should pass through the system until stopping? Final sim is 50,000
+# How many level 2 nodes. This value is updated in the main function
+N = 1
+# How many packets should pass through the system until stopping
 END_PKTS = 50000
-
 # Arrival rates (lambda values) to simulate
 LAMBDAS = range(50, 1250, 50)
 # Average packet length = 1000 bytes/pkt
@@ -63,6 +61,7 @@ class Node:
 
     # 'Peek' at the departure time of the pkt at the front of the queue
     def next_dep(self) -> Union[List[float], None]:
+        global CN
         if len(self.queue) > 0:
             return max(self.queue[0][1], self.prev_pkt[1]) + self.queue[0][0] / CN
         else:
@@ -175,18 +174,15 @@ def run_one_sim(l: int) -> Tuple[Tuple[float, float, float], Tuple[float, float,
     
     # Run a thread that inserts packets into node1 following a P.P.
     # Last arg is lambda (arrival rate)
-    pkt_gen_thread = threading.Thread(target=gen_pkts, name='PktGen',
-                                      args=(node1, node3, l))
+    pkt_gen_thread = threading.Thread(target=gen_pkts, name='PktGen', args=(node1, node3, l))
     pkt_gen_thread.start()
     
     # Create threads to control service and routing at each of the nodes
     # node1 thread
-    node1_thread = threading.Thread(target=node1_func, name='Node1',
-                                    args=(node1, node2_list, node3))
+    node1_thread = threading.Thread(target=node1_func, name='Node1', args=(node1, node2_list, node3))
     node1_thread.start()
     # thread for level 2 nodes
-    level2_thread = threading.Thread(target=level2_func, name='Nodes2x',
-                                    args=(node2_list, node3))
+    level2_thread = threading.Thread(target=level2_func, name='Nodes2x', args=(node2_list, node3))
     level2_thread.start()
     # node3 thread
     node3_thread = threading.Thread(target=node3_func, name='Node3', args=(node3,))
@@ -204,8 +200,8 @@ def run_one_sim(l: int) -> Tuple[Tuple[float, float, float], Tuple[float, float,
 
 # Calculates the average number of pkts and the average delays in nodes 1, 20, and 3
 # Returns two 3-tuples:
-#     - The avg number of pkts in nodes 1, 20, and 3
-#     - The avg delay in nodes 1, 20, and 3
+#     1. The avg number of pkts in nodes 1, 20, and 3
+#     2. The avg delay in nodes 1, 20, and 3
 def calc_avgs(node1: Node, node2: Node, node3: Node) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
     # Sum up the total delays in each node
     sum_delays_1 = 0
@@ -236,7 +232,9 @@ def calc_avgs(node1: Node, node2: Node, node3: Node) -> Tuple[Tuple[float, float
 
 # --------------------- Functions for calculating/plotting averages ----------------------
 # Top-level function to calculate theoretical avgs and plot them with simulated avgs
-def calc_plt_avgs(avg_pkts: List[float], avg_delays: List[float]) -> None:
+# Returns lists of the differences between simulated and theoretical values for # pkts and delays, for each node
+# Structure of each list: [[node1 diffs for each lambda], [node2 diffs], [node3 diffs]]
+def calc_plt_avgs(avg_pkts: List[float], avg_delays: List[float], n: int) -> Tuple[List[List[float]], List[List[float]]]:
     # Calculate theoretical averages by assuming each node is an M/M/1 queue
     # E(n) = expected # pkts in node = rho/(1-rho)
     mu13 = C / PKT_LEN
@@ -260,85 +258,191 @@ def calc_plt_avgs(avg_pkts: List[float], avg_delays: List[float]) -> None:
     node3_delays = [item[2] for item in avg_delays]
     
     # Plot all these lists vs. lambda
-    create_plots(node1_pkts, node2_pkts, node3_pkts, node1_delays, node2_delays,
-                 node3_delays, exp_pkts_13, exp_pkts_2, exp_time_13, exp_time_2)
+    create_avg_plots(node1_pkts, node2_pkts, node3_pkts, node1_delays, node2_delays,
+                     node3_delays, exp_pkts_13, exp_pkts_2, exp_time_13, exp_time_2, n)
+
+    # Calculate the differences in simulated and theoretical values
+    n1_pkts_diff = [sim-cal for sim,cal in zip(node1_pkts, exp_pkts_13)]
+    n2_pkts_diff = [sim-cal for sim,cal in zip(node2_pkts, exp_pkts_2)]
+    n3_pkts_diff = [sim-cal for sim,cal in zip(node3_pkts, exp_pkts_13)]
+    n1_delays_diff = [sim-cal for sim,cal in zip(node1_delays, exp_time_13)]
+    n2_delays_diff = [sim-cal for sim,cal in zip(node2_delays, exp_time_2)]
+    n3_delays_diff = [sim-cal for sim,cal in zip(node3_delays, exp_time_13)]
+    
+    return ([n1_pkts_diff, n2_pkts_diff, n3_pkts_diff], [n1_delays_diff, n2_delays_diff, n3_delays_diff])
     
 
 # Plot final averages, simulated and theoretical
-def create_plots(p1: List[float], p2: List[float], p3: List[float], d1: List[float],
-                 d2: List[float], d3: List[float], en13: List[float], en2: List[float],
-                 et13: List[float], et2: List[float]) -> None:
+def create_avg_plots(p1: List[float], p2: List[float], p3: List[float], d1: List[float],
+                     d2: List[float], d3: List[float], en13: List[float], en2: List[float],
+                     et13: List[float], et2: List[float], n: int) -> None:
     fig, axs = plt.subplots(3, 2)
+    fig.suptitle('N = %d' %(n))
+    
+    # Match line colors to the diffs graph: N=1 blue, N=2 orange, N=3 green, N=4 red
+    if n == 1: c = 'C0'
+    elif n == 2: c = 'C1'
+    elif n == 3: c = 'C2'
+    else: c = 'C3'
+    
     # Node 1 avg # pkts
-    axs[0, 0].plot(LAMBDAS, p1)
-    axs[0, 0].plot(LAMBDAS, en13)
+    axs[0, 0].plot(LAMBDAS, p1, color=c)
+    axs[0, 0].plot(LAMBDAS, en13, color='gray')
     axs[0, 0].set_title('Node 1')
     axs[0, 0].set_xlabel('Lambda (pkts/s)')
     axs[0, 0].set_ylabel('Avg # of pkts')
     axs[0, 0].legend(labels=['Simulated','Theoretical'], loc='upper left')
     # Node 2 avg # pkts
-    axs[1, 0].plot(LAMBDAS, p2)
-    axs[1, 0].plot(LAMBDAS, en2)
+    axs[1, 0].plot(LAMBDAS, p2, color=c)
+    axs[1, 0].plot(LAMBDAS, en2, color='gray')
     axs[1, 0].set_title('Node (2,n)')
     axs[1, 0].set_xlabel('Lambda (pkts/s)')
     axs[1, 0].set_ylabel('Avg # of pkts')
     axs[1, 0].legend(labels=['Simulated','Theoretical'], loc='upper left')
     # Node 3 avg # pkts
-    axs[2, 0].plot(LAMBDAS, p3)
-    axs[2, 0].plot(LAMBDAS, en13)
+    axs[2, 0].plot(LAMBDAS, p3, color=c)
+    axs[2, 0].plot(LAMBDAS, en13, color='gray')
     axs[2, 0].set_title('Node 3')
     axs[2, 0].set_xlabel('Lambda (pkts/s)')
     axs[2, 0].set_ylabel('Avg # of pkts')
     axs[2, 0].legend(labels=['Simulated','Theoretical'], loc='upper left')
     # Node 1 avg delay
-    axs[0, 1].plot(LAMBDAS, d1)
-    axs[0, 1].plot(LAMBDAS, et13)
+    axs[0, 1].plot(LAMBDAS, d1, color=c)
+    axs[0, 1].plot(LAMBDAS, et13, color='gray')
     axs[0, 1].set_title('Node 1')
     axs[0, 1].set_xlabel('Lambda (pkts/s)')
     axs[0, 1].set_ylabel('Avg delay (s)')
     axs[0, 1].legend(labels=['Simulated','Theoretical'], loc='upper left')
     # Node 2 avg delay
-    axs[1, 1].plot(LAMBDAS, d2)
-    axs[1, 1].plot(LAMBDAS, et2)
+    axs[1, 1].plot(LAMBDAS, d2, color=c)
+    axs[1, 1].plot(LAMBDAS, et2, color='gray')
     axs[1, 1].set_title('Node (2,n)')
     axs[1, 1].set_xlabel('Lambda (pkts/s)')
     axs[1, 1].set_ylabel('Avg delay (s)')
     axs[1, 1].legend(labels=['Simulated','Theoretical'], loc='upper left')
     # Node 3 avg delay
-    axs[2, 1].plot(LAMBDAS, d3)
-    axs[2, 1].plot(LAMBDAS, et13)
+    axs[2, 1].plot(LAMBDAS, d3, color=c)
+    axs[2, 1].plot(LAMBDAS, et13, color='gray')
     axs[2, 1].set_title('Node 3')
     axs[2, 1].set_xlabel('Lambda (pkts/s)')
     axs[2, 1].set_ylabel('Avg delay (s)')
     axs[2, 1].legend(labels=['Simulated','Theoretical'], loc='upper left')
     
     plt.tight_layout()
-    plt.show()
+    
+
+# Plot differences in final averages, simulated minus theoretical
+def create_diff_plots(pkts_diff: List[List[List[float]]], delays_diff:List[List[List[float]]]) -> None:
+    fig1, axs1 = plt.subplots(2, 1)
+    fig2, axs2 = plt.subplots(2, 1)
+    fig3, axs3 = plt.subplots(2, 1)
+    
+    # Node 1 difference in average number of packets
+    axs1[0].plot(LAMBDAS, pkts_diff[0][0])
+    axs1[0].plot(LAMBDAS, pkts_diff[1][0])
+    axs1[0].plot(LAMBDAS, pkts_diff[2][0])
+    axs1[0].plot(LAMBDAS, pkts_diff[3][0])
+    axs1[0].set_title('Node 1: Simulated - Theoretical Values')
+    axs1[0].set_xlabel('Lambda (pkts/s)')
+    axs1[0].set_ylabel('Difference in # of pkts')
+    axs1[0].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    # Node 1 difference in delay
+    axs1[1].plot(LAMBDAS, delays_diff[0][0])
+    axs1[1].plot(LAMBDAS, delays_diff[1][0])
+    axs1[1].plot(LAMBDAS, delays_diff[2][0])
+    axs1[1].plot(LAMBDAS, delays_diff[3][0])
+    axs1[1].set_xlabel('Lambda (pkts/s)')
+    axs1[1].set_ylabel('Difference in delay (s)')
+    axs1[1].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    
+    # Node 2 difference in average number of packets
+    axs2[0].plot(LAMBDAS, pkts_diff[0][1])
+    axs2[0].plot(LAMBDAS, pkts_diff[1][1])
+    axs2[0].plot(LAMBDAS, pkts_diff[2][1])
+    axs2[0].plot(LAMBDAS, pkts_diff[3][1])
+    axs2[0].set_title('Node (2,n): Simulated - Theoretical Values')
+    axs2[0].set_xlabel('Lambda (pkts/s)')
+    axs2[0].set_ylabel('Difference in # of pkts')
+    axs2[0].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    # Node 2 difference in delay
+    axs2[1].plot(LAMBDAS, delays_diff[0][1])
+    axs2[1].plot(LAMBDAS, delays_diff[1][1])
+    axs2[1].plot(LAMBDAS, delays_diff[2][1])
+    axs2[1].plot(LAMBDAS, delays_diff[3][1])
+    axs2[1].set_xlabel('Lambda (pkts/s)')
+    axs2[1].set_ylabel('Difference in delay (s)')
+    axs2[1].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    
+    # Node 3 difference in average number of packets
+    axs3[0].plot(LAMBDAS, pkts_diff[0][2])
+    axs3[0].plot(LAMBDAS, pkts_diff[1][2])
+    axs3[0].plot(LAMBDAS, pkts_diff[2][2])
+    axs3[0].plot(LAMBDAS, pkts_diff[3][2])
+    axs3[0].set_title('Node 3: Simulated - Theoretical Value')
+    axs3[0].set_xlabel('Lambda (pkts/s)')
+    axs3[0].set_ylabel('Difference in # of pkts')
+    axs3[0].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    # Node 3 difference in delay
+    axs3[1].plot(LAMBDAS, delays_diff[0][2])
+    axs3[1].plot(LAMBDAS, delays_diff[1][2])
+    axs3[1].plot(LAMBDAS, delays_diff[2][2])
+    axs3[1].plot(LAMBDAS, delays_diff[3][2])
+    axs3[1].set_xlabel('Lambda (pkts/s)')
+    axs3[1].set_ylabel('Difference in delay (s)')
+    axs3[1].legend(labels=['N = 1','N = 2','N = 3', 'N = 4'], loc='upper left')
+    
+    plt.tight_layout()
 # ----------------------------------------------------------------------------------------
 
 
 
 # ------------------------------------ Main function -------------------------------------
+# Runs simulations one at a time for each N value with all lambda values (arrival rates)
 def main():
-    # Lists for average # pkts in nodes 1, 20, and 3, and avg delay in nodes 1, 20, and 3
-    # Structure: lists of 3-tuples
-    # [ (_, _, _) , (_, _, _) , ... , (_, _, _) ]
-    #     l=50        l=100     ...    l=1200
-    avg_pkts_list = []
-    avg_delays_list = []
+    global N
+    global CN
     
-    # Run simulation for each of lambda (arrival rate) = 50, 100, 150, ..., 1150, 1200
-    for l in LAMBDAS:
-        print('-------------------------- SIMULATION', l, '--------------------------')
-        # Run a simulation with this lambda (pkt arrival rate)
-        pkts, delays = run_one_sim(l)
-        # Store avg number of pkts and avg delays in the lists
-        avg_pkts_list.append(pkts)
-        avg_delays_list.append(delays)
+    # Accumulate avgs for # pkts and delays, for all N values and all lambda values
+    # Also accumulate the differences between simulated and theoretical values into lists
+    # List of lists: [[avgs for N=1], ..., [avgs for N=4]]
+    avg_pkts_all_N = []
+    avg_delays_all_N = []
+    # List of lists: [[diffs for N=1], ..., [diffs for N=4]]
+    pkts_diff_all_N = []
+    delays_diff_all_N = []
     
-    # Plot the simulated and theoretical averages for each lambda
-    calc_plt_avgs(avg_pkts_list, avg_delays_list)
+    # Run simulation for each of N = 1,2,3,4
+    for N in range(1,5):
+        print('N = %d' %(N))
+        CN = 1250000 / N
+        # List of averages for this N; is a list of 3-tuples
+        # Structure: [ (_, _, _) , (_, _, _) , ... , (_, _, _) ]
+        #                l=50        l=100     ...    l=1200
+        avg_pkts_one_N = []
+        avg_delays_one_N = []
+        # Run simulation for each of lambda (arrival rate) = 50, 100, 150, ..., 1150, 1200
+        for l in LAMBDAS:
+            print('--------------------- SIMULATION', l, '---------------------')
+            # Run a simulation with this lambda (pkt arrival rate)
+            pkts, delays = run_one_sim(l)
+            # Store avg number of pkts and avg delays in the lists
+            avg_pkts_one_N.append(pkts)
+            avg_delays_one_N.append(delays)
+        
+        # Store avg lists for this N in the 'parent' lists that keep track of all N = 1,2,3,4
+        avg_pkts_all_N.append(avg_pkts_one_N)
+        avg_delays_all_N.append(avg_delays_one_N)
 
+    # Plot simulated and theoretical averages
+    # and store differences between simulated and theoretical averages in a list
+    for i in range(len(avg_pkts_all_N)):
+        pkts_diff_one_N, delays_diff_one_N = calc_plt_avgs(avg_pkts_all_N[i], avg_delays_all_N[i], i+1)
+        pkts_diff_all_N.append(pkts_diff_one_N)
+        delays_diff_all_N.append(delays_diff_one_N)
+    
+    # Plot differences between simulated and theoretical averages
+    create_diff_plots(pkts_diff_all_N, delays_diff_all_N)
+    plt.show()
 # ----------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
